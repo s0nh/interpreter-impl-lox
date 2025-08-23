5cc07f46ad3adb131eb09ee2c1f63ad5f610ec38
s0nh <wlq8112@hanyang.ac.kr>
2025-08-24 01:33:28 +0900

    ch14-1문제: RLE로 명령어 line 자료 압축

--- clox/chunk.h
+++ clox/chunk.h
@@ -15,10 +15,17 @@ typedef enum {
 } OpCode;
 
 typedef struct {
+    int line;
+    int count;
+} Line;
+
+typedef struct {
     int count;
     int capacity;
+    int lineCnt;
+    int lineCap;
     uint8_t* code;
-    int* lines;
+    Line* lines;
     ValueArray constants;
 } Chunk;
 
@@ -26,5 +33,6 @@ void initChunk(Chunk* chunk);
 void freeChunk(Chunk* chunk);
 void writeChunk(Chunk* chunk, uint8_t byte, int line);
 int addConstant(Chunk* chunk, Value value);
+int getLine(Chunk* chunk, int index);
 
 #endif
--- clox/main.c
+++ clox/main.c
@@ -14,21 +14,22 @@ int main(int argc, const char* argv[]) {
     writeChunk(&chunk, constant, 123);
     
     constant = addConstant(&chunk, 3.4);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
+    writeChunk(&chunk, OP_CONSTANT, 124);
+    writeChunk(&chunk, constant, 124);
 
-    writeChunk(&chunk, OP_ADD, 123);
+    writeChunk(&chunk, OP_ADD, 124);
 
     constant = addConstant(&chunk, 5.6);
-    writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constant, 123);
+    writeChunk(&chunk, OP_CONSTANT, 125);
+    writeChunk(&chunk, constant, 125);
 
-    writeChunk(&chunk, OP_DIVIDE, 123);
-    writeChunk(&chunk, OP_NEGATE, 123);
+    writeChunk(&chunk, OP_DIVIDE, 126);
+    writeChunk(&chunk, OP_NEGATE, 127);
 
-    writeChunk(&chunk, OP_RETURN, 123);
+    writeChunk(&chunk, OP_RETURN, 128);
 
     disassembleChunk(&chunk, "test chunk");
+
     interpret(&chunk);
     freeVM();
     freeChunk(&chunk);
