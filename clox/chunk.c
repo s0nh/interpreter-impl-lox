352624ad0f87055f9a3a0b78eed67f1a9b03e331
s0nh <wlq8112@hanyang.ac.kr>
2025-08-24 01:33:28 +0900

    ch14-1번 문제

--- clox/chunk.c
+++ clox/chunk.c
@@ -6,6 +6,8 @@
 void initChunk(Chunk* chunk) {
     chunk->count = 0;
     chunk->capacity = 0;
+    chunk->lineCnt = 0;
+    chunk->lineCap = 0;
     chunk->code = NULL;
     chunk->lines = NULL;
     initValueArray(&chunk->constants);
@@ -18,18 +20,41 @@ void freeChunk(Chunk* chunk) {
     initChunk(chunk);
 }
 
+int getLine(Chunk* chunk, int index) {
+    int count = index + 1;
+    for (int i=0; i<chunk->lineCnt; i++) {
+        if(chunk->lines[i].count >= count) {
+            return chunk->lines[i].line;
+        }
+
+        count -= chunk->lines[i].count;
+    }
+}
+
 void writeChunk(Chunk* chunk, uint8_t byte, int line) {
     if (chunk->capacity < chunk->count + 1) {
         int oldCapacity = chunk->capacity;
         chunk->capacity = GROW_CAPACITY(oldCapacity);
         chunk->code = GROW_ARRAY(uint8_t, chunk->code,
             oldCapacity, chunk->capacity);
-        chunk->lines = GROW_ARRAY(int, chunk->lines, 
-            oldCapacity, chunk->capacity);
+    }
+
+    if (chunk->lineCap < chunk->lineCnt + 1) {
+        int oldCapacity = chunk->lineCap;
+        chunk->lineCap = GROW_CAPACITY(oldCapacity);
+        chunk->lines = GROW_ARRAY(Line, chunk->lines, 
+            oldCapacity, chunk->lineCap);
     }
 
     chunk->code[chunk->count] = byte;
-    chunk->lines[chunk->count] = line;
+
+    if (chunk->lineCnt == 0 || line != chunk->lines[chunk->lineCnt-1].line) {
+        (chunk->lines[chunk->lineCnt]).line = line;
+        (chunk->lines[chunk->lineCnt]).count = 0;
+        chunk->lineCnt++;
+    }
+    
+    (chunk->lines[chunk->lineCnt-1]).count++;
     chunk->count++;
 }
 
